ef56d2fc9b7a05f480433b8602ea942da9daf343
BOWEN2491 <160082600+BOWEN2491@users.noreply.github.com>
2026-06-05 17:16:08 +0800

    6.5

--- twitch/src/main/java/com/laioffer/twitch/TwitchApplication.java
+++ twitch/src/main/java/com/laioffer/twitch/TwitchApplication.java
@@ -1,13 +1,21 @@
-package com.laioffer.twitch.hello;
+package com.laioffer.twitch;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class TwitchApplication {
 
+
     public static void main(String[] args) {
+        System.setProperty("java.net.useSystemProxies",
+                System.getProperty("java.net.useSystemProxies", "true"));
         SpringApplication.run(TwitchApplication.class, args);
     }
+
 
 }
